1529980ac9f6d0dc32ba36d6073a0432f40a4644
NguyenHuyenTrang070899 <37436902+NguyenHuyenTrang070899@users.noreply.github.com>
2020-12-30 14:53:55 +0700

    Update ThreadCreate.java

--- ThreadCreate.java
+++ ThreadCreate.java
@@ -4,9 +4,9 @@ import java.util.List;
 public class ThreadCreate {
 	public void startThread(File file) {
 		Threading t1 = new Threading("B.txt");
-		Threading t1 = new Threading("B.txt");
+		Threading t1 = new Threading("C.txt");
 		Thread td1 = new Thread(t1);
-		Thread td1 = new Thread(t1);
+		Thread td2 = new Thread(t2);
 		td1.start();
 		td2.start();
 		
